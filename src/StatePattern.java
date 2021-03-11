eb6e25eacc787928c643e543148003bba5066b28
Isichei Phelim <isicheiphelim@gmail.com>
2021-03-11 21:52:48 +0000

    Added the statedesign pattern

---
+++ src/StatePattern.java
@@ -0,0 +1,12 @@
+public class StatePattern {
+    public static void main(String[] args)
+    {
+        AlertStateContext stateContext = new AlertStateContext();
+        stateContext.alert();
+        stateContext.alert();
+        stateContext.setState(new Silent());
+        stateContext.alert();
+        stateContext.alert();
+        stateContext.alert();
+    }
+}
